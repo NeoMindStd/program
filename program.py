c1b9889a9c8b51daceb2a638bc76abe1b2b75ca6
NeoMindStd <dwj1996@naver.com>
2023-10-17 03:00:56 +0900

    일부데이터 누락시에도 정상동작하도록 수정

--- program.py
+++ program.py
@@ -3,28 +3,25 @@ import pandas as pd
 from tkinter import Tk, Listbox, Button, Entry, Label, Radiobutton, StringVar, messagebox
 from datetime import datetime
 
+#TICKERS = ['AAPL', 'MSFT']
+TICKERS = ['AAPL', 'MSFT', 'AMZN', '035420.KS']
+INFO_KEYS = ['symbol', 'shortName', 'exchange', 'country', 'marketCap', 'currency', 'sharesOutstanding',
+        'trailingPE', 'forwardPE', 'enterpriseToEbitda', 'returnOnEquity', 'priceToBook',
+        'earningsQuarterlyGrowth', 'sector', 'trailingAnnualDividendYield']
+
 def get_stock_data(tickers):
     data = []
     for ticker in tickers:
+        row_data = []
         stock = yf.Ticker(ticker)
         info = stock.info
-        data.append([
-            info['symbol'],
-            info['shortName'],
-            info['exchange'],
-            info['country'],
-            info['marketCap'],
-            info['currency'],
-            info['sharesOutstanding'],
-            info['trailingPE'],
-            info['forwardPE'],
-            info['enterpriseToEbitda'],
-            info['returnOnEquity'],
-            info['priceToBook'],
-            info['earningsQuarterlyGrowth'],
-            info['sector'],
-            info['trailingAnnualDividendYield']
-        ])
+        for key in INFO_KEYS:
+            try:
+                row_data.append(info[key])
+            except Exception as e:
+                print(f"caught except: {ticker} - {e}")
+                row_data.append('N/A')
+        data.append(row_data)
     return pd.DataFrame(data, columns=[
         '티커', '종목명', '거래소', '국가', '시가총액', '화폐',
         '유통 주식수', 'trailingPER (직전 12개월)', 'forwardPER (직후 12개월 추정)',
@@ -38,45 +35,14 @@ def export_to_excel(data):
     return filename
 
 def on_export():
-    selected_markets = [listbox.get(idx) for idx in listbox.curselection()]
-    tickers = ['AAPL', 'MSFT']  # You would need to obtain a list of tickers for the selected markets
+    tickers = TICKERS  # You would need to obtain a list of tickers for the selected markets
     data = get_stock_data(tickers)
-
-    # 필터링
-    if per_value.get():
-        filter_value = float(per_value.get())
-        filter_direction = per_direction.get()
-        if filter_direction == 'Below':
-            data = data[data['trailingPER (직전 12개월)'] <= filter_value]
-        else:
-            data = data[data['trailingPER (직전 12개월)'] >= filter_value]
-
-    # ... 같은 방식으로 다른 지표들에 대해 필터링 ...
 
     filename = export_to_excel(data)
     messagebox.showinfo("Export Complete", f"Data exported to {filename}")
 
 root = Tk()
-root.title("program")
-
-listbox = Listbox(root, selectmode='extended')  # 복수 선택을 위해 'extended'로 설정
-listbox.insert('end', 'NASDAQ')
-listbox.insert('end', 'KOSDAQ')
-# ... add other markets
-listbox.pack()
-
-# 지표 필터링 입력
-per_label = Label(root, text="P/E Ratio")
-per_label.pack()
-per_value = Entry(root)
-per_value.pack()
-per_direction = StringVar(value='Below')
-per_below = Radiobutton(root, text="Below", variable=per_direction, value='Below')
-per_below.pack()
-per_above = Radiobutton(root, text="Above", variable=per_direction, value='Above')
-per_above.pack()
-
-# ... 같은 방식으로 다른 지표들에 대한 입력 위젯 추가 ...
+root.title("주식 데이터 파서")
 
 export_button = Button(root, text="Export to Excel", command=on_export)
 export_button.pack()
